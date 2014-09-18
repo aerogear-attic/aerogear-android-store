70fefce1c366d4e7c0f7bc89804278f374f708d7
Daniel Passos <daniel@passos.me>
2014-09-18 13:58:34 -0300

    Force DataManager to return a typed Store

--- aerogear-android-store/src/org/jboss/aerogear/android/DataManager.java
+++ aerogear-android-store/src/org/jboss/aerogear/android/DataManager.java
@@ -72,8 +72,8 @@ public final class DataManager {
 
     }
 
-    public static Store getStore(String name) {
-        return stores.get(name);
+    public static <T> Store<T> getStore(String name) {
+        return (Store<T>) stores.get(name);
     }
 
 }
